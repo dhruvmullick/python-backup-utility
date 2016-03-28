affaeb39bf06979f87ee1bae8137c683974d32ad
Dhruv Mullick <dmullick95@gmail.com>
2016-03-28 10:28:47 +0530

    Added GUI. Must change diff backup

--- GUI.py
+++ GUI.py
@@ -1,41 +1,132 @@
-#!/usr/bin/python
+import sys
 from Tkinter import *
-dirtext='Select your pictures folder'
-filetext= 'Select your exception file'
 import Tkconstants, tkFileDialog
-
+import datetime
 import ttk
-def askdirectory():
+import main
 
-    dirname = tkFileDialog.askdirectory(parent=root, title=dirtext)
-    b1["text"] = str(dirname) if dirname else dirtext
+dirtext = 'Select your pictures folder'
+filetext = 'Select your exception file'
+dirtext1 = 'Select destination of image'
 
-def openFile():
-    filename = tkFileDialog.askopenfilename(parent=root,title=filetext) ## filename not filehandle
-    filebut["text"]= str(filename) if filename else filetext
-
+# file and directory function
 root = Tk()
+Ldir = []
+Ldirignore = []
+dirn = []
+ldir1 = []
+dirname2=""
+
+def askdirectory():
+    global Ldir
+    dirname = tkFileDialog.askdirectory(parent=root, title=dirtext)
+    # btn["text"] =str(btn["text"]+str(dirname)) if dirname else dirtext
+    print dirname
+    T.insert(INSERT, dirname+"\n")
+    Ldir += [(dirname)]
+
+
+def openfile():
+    global Ldir
+    filename = tkFileDialog.askopenfilename(parent=root, title=filetext)  # filename not filehandle
+    #  filebut["text"]= str(filename) if filename else filetext
+    print filename
+    T.insert(INSERT, filename+"\n")
+    Ldir +=[(filename)]
+
+
+def askdirectory1():
+    global ldir1
+    dirname1 = tkFileDialog.askdirectory(parent=root, title=dirtext)
+    print dirname1
+    T4.insert(INSERT, dirname1+"\n")
+    ldir1 += [(dirname1)]
+
+
+def openfile1():
+    global ldir1
+    filename1 = tkFileDialog.askopenfilename(parent=root, title=filetext)
+    print filename1
+    T4.insert(INSERT, filename1+"\n")
+    ldir1 += [(filename1)]
+
+
+def askd1():
+    global dirn
+    dirname = tkFileDialog.askdirectory(parent=root, title=dirtext1)
+    T2.insert(INSERT, dirname+"\n")
+    dirn += [(dirname)]
+    # Dest["text"] =str(dirn) if dirn else dirtext
+
+def  DestSelect():
+    global dirname2
+    dirname2 = tkFileDialog.askdirectory(parent=root, title=dirtext1)
+
+def Callfun():
+    global Ldir,dirname2,ldir1
+    main.Backupnow(Ldir,dirname2,[])
+
 root.title('Backup Implementation')
-#the notebook with three pages
+#   *** NOTEBOOK***
 n = ttk.Notebook(root)
-f1 = Frame(n)  # first page, which would get widgets gridded into it
+# fstatusr=Frame(root)
+# fstatusr.grid(sticky=S+W)
 
+# fstatusl=Frame(root)
+# fstatusl.grid(sticky=S+E)
+f1 = Frame(n, bd=1, relief=SUNKEN)  # first page, which would get widgets gridded into it
 f2 = Frame(n)  # second page
-
 f3 = Frame(n)
+f4 = Frame(n)
+
 n.add(f1, text='Backup files')
-n.add(f2, text='Exception files')
+n.add(f2, text='Ignore List')
 n.add(f3, text='History')
+n.add(f4, text='ISO image')
 n.pack(fill=BOTH)
-label_1 = Label(f1, text="Backup Files")
-label_1.grid(row=0)
-entry_1 = Entry(f1)
-entry_1.grid(row=0,column=2,sticky=W+S+E+N)
-b1=Button(f1,text='Browse',command=askdirectory)
-b1.grid(row=1,column=0)
-filebut=Button(f2,text='Browse exception files',command=openFile)
-filebut.grid(row=1,column=0)
-#browse button
-#labels to specify what to do
+
+T = Text(f1)
+T.pack()
+b1 = Button(f1, text="Browse directories", command=askdirectory)
+b1.pack()
+b2 = Button(f1, text="Browse files", command=openfile)
+b2.pack()
+T2 = Text(f4, height=10)
+T2.pack()
+T2.insert(INSERT, "Choose the destination where you want your ISO image to be ceated \n")
+b10 = Button(f4, text="get ISO image", command=askd1)
+b10.pack()
+Destdir = Button(f1, text="Select Backup Destination", command=DestSelect)
+Destdir.pack()
+BackupNow = Button(f1, text="Backup Now", command=Callfun)
+b2.pack()
+
+
+#  *** STATUS BAR ***
+now = datetime.datetime.now()
+status1 = Label(root, text= "Preparing to Backup...",bd=1,relief=SUNKEN, anchor=W)
+status1.pack(side=BOTTOM, fill=X)
+#  enter the present time and date at the right
+
+str = '%s : %s' %(now.hour, now.minute)
+
+status2 = Label(root, text=str, bd=1,relief=SUNKEN, anchor=E)  # create a function so that the status bar#  changes text evry time a button is clicked
+status2.pack(side=BOTTOM, fill=X)  # enter the present time and date at the right
+T4 = Text(f2, height=20)
+T4.pack()
+b11 = Button(f2, text="Browse directories to ignore", command=askdirectory1)
+b11.pack()
+b21 = Button(f2, text="Browse files to ignore", command=openfile1)
+b21.pack()
+lst1 = ['.pdf', '.txt', '.rtf', '.ppt']
+var1 = StringVar()
+var1.set('select from the options below')
+drop = OptionMenu(f2, var1, *lst1)
+drop.grid(row=15)
+drop.pack()
 
 root.mainloop()
+#  check whether the last char
+#  check file and directory and extensions.. Backup now button
+#  destination field
+#  drop down custom input
--- main.py
+++ main.py
@@ -2,35 +2,44 @@
 Python file backup utility
 """
 
-import copyFiles
 import diffBackup
 import createISO
 import clean
 
 # "src = "/Users/dhruvmullick/CS/Project Work/python-backup-utility/srcFolder/python-backup-utility/main.py"
-src = "/Users/dhruvmullick/CS/Project Work/python-backup-utility/srcFolder/"
-dst = "/Users/dhruvmullick/CS/Project Work/python-backup-utility/destFolder/"
+# src = "/Users/dhruvmullick/CS/Project Work/python-backup-utility/srcFolder/"
+# dst = "/Users/dhruvmullick/CS/Project Work/python-backup-utility/destFolder/"
 
 
 # ignoreList = {".pdf":1} #write paths of files, folders and extensions here which we don't want to copy
 # ignoreList = {"/Users/dhruvmullick/CS/Project Work/python-backup-utility/srcFolder/gaddaarChetan.py":1}
-ignoreList = {"/Users/dhruvmullick/CS/Project Work/python-backup-utility/srcFolder/MyFolder":1}
+# ignoreList = {"/Users/dhruvmullick/CS/Project Work/python-backup-utility/srcFolder/MyFolder":1}
 # ignoreList={}
 
 
-# Make a completely fresh backup
-#delete the files already there at dst, as this is a fresh backup.
-#
-# clean.readyDst(dst)
-# copyFiles.copyFull(src,dst,ignoreList)
+def backupNow(L,dst, ignoreList):
 
-# Make a differential backup
-diffBackup.mainDiffBackup(src,dst,ignoreList)
+    # Make a completely fresh backup
+    #delete the files already there at dst, as this is a fresh backup.
+    #
+    # clean.readyDst(dst)
+    # copyFiles.copyFull(src,dst,ignoreList)
 
-# Create an isoimage of the backup folder
-isodstfolder = "/Users/dhruvmullick/CS/Project Work/python-backup-utility/"    #folder where iso image is to be saved. Take input from user.
-backupdst=dst   # Folder of which we have to make backup
-# createISO.isoCreator(backupdst,isodstfolder)
+    # Make a differential backup
+
+    for src in L:
+        print src
+    print dst
+
+        # diffBackup.mainDiffBackup(src,dst,ignoreList)
+
+    # Create an isoimage of the backup folder
+    # isodstfolder = "/Users/dhruvmullick/CS/Project Work/python-backup-utility/"    #folder where iso image is to be saved. Take input from user.
+    # backupdst=dst   # Folder of which we have to make backup
+    # createISO.isoCreator(backupdst,isodstfolder)
+    print "\n\nBackup has been created successfully. Thank you. \n\n"
 
 
-print "\n\nBackup has been created successfully. Thank you. \n\n"
+    for src in L:
+        print src
+    print dst
